69721b3218b04bdeac38e38c0c38dbaecb939c90
Felipe Renan <felipeferreira3146@gmail.com>
2025-03-04 22:52:19 -0300

    feat: inicia configuração do DbContext e altera model Venda
    Configura OnModelCreating de Produto e adiciona atributos DataVenda e PrecoTotalVenda de Venda

--- Desktop-GerenciadorDeEstoque-WPF/Core/Model/Venda.cs
+++ Desktop-GerenciadorDeEstoque-WPF/Core/Model/Venda.cs
@@ -5,4 +5,7 @@ public class Venda
     public int Id { get; set; }
     public int Quantidade { get; set; }
     public Produto Produto { get; set; }
+    public DateTime DataVenda { get; set; }
+    public decimal PrecoTotalVenda { get; set; }
+    
 }
--- Desktop-GerenciadorDeEstoque-WPF/Data/AppDbContext.cs
+++ Desktop-GerenciadorDeEstoque-WPF/Data/AppDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using Desktop_GerenciadorDeEstoque_WPF.Core.Model;
 using Microsoft.EntityFrameworkCore;
-using Material = System.Windows.Media.Media3D.Material;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
@@ -18,7 +17,8 @@ public class AppDbContext : DbContext
             .AddJsonFile("appsettings.json")
             .Build();
 
-        _connectionString = configuration.GetConnectionString("PostgresConnection");
+        _connectionString = configuration.GetConnectionString("DefaultConnection") 
+                            ?? throw new InvalidOperationException("A string de conexão não foi encontrada.");
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -26,8 +26,39 @@ public class AppDbContext : DbContext
         optionsBuilder.UseNpgsql(_connectionString);
     }
     
-    public DbSet<Produto> Produtos;
-    public DbSet<Usuario> Usuarios;
-    public DbSet<TransacaoFinanceira> TransacoesFinanceiras;
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<TransacaoFinanceira> TransacoesFinanceiras { get; set; }
     public DbSet<Material> Materiais { get; set; }
+    public DbSet<Venda> Vendas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Produto>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+            entity.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            entity.Property(p => p.Nome).IsRequired().HasMaxLength(200);
+            entity.Property(p => p.ValorVenda).HasColumnType("decimal(10,2)");
+        });
+        
+        
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
