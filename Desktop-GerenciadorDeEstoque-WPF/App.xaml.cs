3302f2de905789b68da94642ad3efac18a9fd980
Felipe Renan <felipeferreira3146@gmail.com>
2025-03-05 01:25:01 -0300

    feat: adiciona configuração de injeção de dependência ao App.xaml.cs

--- Desktop-GerenciadorDeEstoque-WPF/App.xaml.cs
+++ Desktop-GerenciadorDeEstoque-WPF/App.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using CadastroProdutosWPF;
+using Desktop_GerenciadorDeEstoque_WPF.Core.Services;
+using Desktop_GerenciadorDeEstoque_WPF.Core.Services.Interfaces;
+using Desktop_GerenciadorDeEstoque_WPF.Data;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Desktop_GerenciadorDeEstoque_WPF;
 
@@ -9,4 +15,25 @@ namespace Desktop_GerenciadorDeEstoque_WPF;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>();
+
+        services.AddScoped<IProdutoService, ProdutoService>();
+
+        services.AddTransient<MainWindow>();
+    }
 }
