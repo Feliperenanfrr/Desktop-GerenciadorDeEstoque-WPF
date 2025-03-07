dfca2635020524525daafae59a4fa822eda7ec0c
Felipe Renan <felipeferreira3146@gmail.com>
2025-03-07 10:23:39 -0300

    refactor: cria migration para alterar o nome da tabela

---
+++ Desktop-GerenciadorDeEstoque-WPF/Migrations/20250307132152_RenomearTransacoesFinanceirasParaFinanceiro.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Desktop_GerenciadorDeEstoque_WPF.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenomearTransacoesFinanceirasParaFinanceiro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "TransacoesFinanceiras",
+                newName: "Financeiro"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Financeiro",
+                newName: "TransacoesFinanceiras"
+            );
+
+        }
+    }
+}
