b3ff09df017a59512ae43bdad3e3f50fa51d81c9
Felipe Renan <felipeferreira3146@gmail.com>
2025-03-07 22:00:51 -0300

    feat: cria DashboardViewModel e instala LiveCharts para gerar gráficos

--- Desktop-GerenciadorDeEstoque-WPF/Core/ViewModel/DashboardViewModel.cs
+++ Desktop-GerenciadorDeEstoque-WPF/Core/ViewModel/DashboardViewModel.cs
@@ -1,32 +1,87 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using Desktop_GerenciadorDeEstoque_WPF.Core.Model;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using Desktop_GerenciadorDeEstoque_WPF.Core.Services;
-using Desktop_GerenciadorDeEstoque_WPF.Data;
+using Desktop_GerenciadorDeEstoque_WPF.Core.Model;
+using LiveCharts;
+using LiveCharts.Wpf;
 
-namespace Desktop_GerenciadorDeEstoque_WPF.Core.ViewModel;
-
-public class DashboardViewModel : INotifyPropertyChanged
+namespace Desktop_GerenciadorDeEstoque_WPF.Core.ViewModel
 {
-    static AppDbContext  _context;
-    private ObservableCollection<Financeiro> _transacoes = new ();
-    private FinanceiroService _financeiroService = new FinanceiroService(_context);
+    public class DashboardViewModel
+    {
+        private readonly FinanceiroService _financeiroService;
+        private readonly VendaService _vendaService;
+        private readonly ProdutoService _produtoService;
 
-    public ObservableCollection<Financeiro> Transacoes
-    {
-        get { return _transacoes; }
-        set { _transacoes = value; OnPropertyChanged(nameof(Transacoes)); }
-    }
-    
-    public void AdicionarTransacao(Financeiro transacao)
-    {
-        _financeiroService.CriarTransacao(transacao);
-        Transacoes.Add(transacao);
-    }
+        public SeriesCollection FinanceiroChart { get; set; }
+        public SeriesCollection VendasChart { get; set; }
+        public SeriesCollection ProdutosChart { get; set; }
+        
+        public DashboardViewModel(FinanceiroService financeiroService, VendaService vendaService, ProdutoService produtoService)
+        {
+            _financeiroService = financeiroService;
+            _vendaService = vendaService;
+            _produtoService = produtoService;
+            
+            CarregarGraficos();
+        }
 
-    public event PropertyChangedEventHandler PropertyChanged;
-    protected void OnPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        private void CarregarGraficos()
+        {
+            CarregarGraficoFinanceiro();
+            CarregarGraficoVendas();
+            CarregarGraficoProdutos();
+        }
+
+        private void CarregarGraficoFinanceiro()
+        {
+            var transacoes = _financeiroService.ListarTransacoes();
+            var entradas = transacoes.Where(t => !t.Tipo).Sum(t => t.Valor);
+            var saidas = transacoes.Where(t => t.Tipo).Sum(t => t.Valor);
+
+            FinanceiroChart = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Entradas",
+                    Values = new ChartValues<decimal> { entradas }
+                },
+                new ColumnSeries
+                {
+                    Title = "Saídas",
+                    Values = new ChartValues<decimal> { saidas }
+                }
+            };
+        }
+
+        private void CarregarGraficoVendas()
+        {
+            var vendas = _vendaService.ListarVendas();
+            var totalVendas = vendas.Sum(v => v.PrecoTotalVenda);
+            
+            VendasChart = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Vendas",
+                    Values = new ChartValues<decimal> { totalVendas }
+                }
+            };
+        }
+
+        private void CarregarGraficoProdutos()
+        {
+            var produtos = _produtoService.ListarProdutos();
+            
+            ProdutosChart = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Produtos em Estoque",
+                    Values = new ChartValues<int>(produtos.Select(p => p.Quantidade))
+                }
+            };
+        }
     }
 }
