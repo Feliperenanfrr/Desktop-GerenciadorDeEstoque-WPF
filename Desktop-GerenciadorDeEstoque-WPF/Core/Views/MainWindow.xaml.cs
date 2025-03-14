772909e2c13f144fbf1110dc7f759746daed9d74
Felipe Renan <felipeferreira3146@gmail.com>
2025-03-14 10:49:14 -0300

    feat: adiciona responsividade na MainWindow e comandos para fechar, maximizar e minimizar

--- Desktop-GerenciadorDeEstoque-WPF/Core/Views/MainWindow.xaml.cs
+++ Desktop-GerenciadorDeEstoque-WPF/Core/Views/MainWindow.xaml.cs
@@ -9,7 +9,30 @@ namespace Desktop_GerenciadorDeEstoque_WPF.Core.Views
         {
             InitializeComponent();
             DataContext = mainViewModel;
+            this.MouseLeftButtonDown += (s, e) => DragMove();
         }
 
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
+
 }
