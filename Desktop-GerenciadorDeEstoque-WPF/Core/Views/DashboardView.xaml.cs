b2b882092d98c6da0cdf99afcffdebeda794655a
Felipe Renan <felipeferreira3146@gmail.com>
2025-03-14 10:32:14 -0300

    feat: transforma DashboardView em UserControl e configura na MainWindow

--- Desktop-GerenciadorDeEstoque-WPF/Core/Views/DashboardView.xaml.cs
+++ Desktop-GerenciadorDeEstoque-WPF/Core/Views/DashboardView.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using Desktop_GerenciadorDeEstoque_WPF.Core.ViewModel;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Desktop_GerenciadorDeEstoque_WPF.Core.Views
 {
-    public partial class DashboardView : Window
+    public partial class DashboardView : UserControl
     {
         public DashboardView()
         {
